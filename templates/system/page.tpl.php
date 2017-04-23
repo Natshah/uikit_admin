cdb8b38f1e8d5ff7a44180b8a50a86557dc064ea
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-04-23 07:33:02 -0400

    Theme views table and put action links into uk-subnav

--- templates/system/page.tpl.php
+++ templates/system/page.tpl.php
@@ -109,7 +109,7 @@
     <?php print render($page['help']); ?>
 
     <?php if ($action_links): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
+      <ul class="uk-subnav action-links"><?php print render($action_links); ?></ul>
     <?php endif; ?>
 
     <?php print render($page['content']); ?>
