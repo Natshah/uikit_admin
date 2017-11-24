7c0217bfdbb56f5d131ed72cd74c6902467e52c6
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-11-23 19:04:25 -0500

    Updated uikit_admin.theme.js behaviors

--- js/uikit_admin.theme.js
+++ js/uikit_admin.theme.js
@@ -6,35 +6,39 @@
 (function ($, Drupal) {
   'use strict';
 
-  var statusMessagesRegion = $('#region--status_messages');
-  var successMessages = statusMessagesRegion.find('.uk-alert-success');
-  var warningMessages = statusMessagesRegion.find('.uk-alert-warning');
-  var dangerMessages = statusMessagesRegion.find('.uk-alert-danger');
+  Drupal.behaviors.uikitAdminStatusMessages = {
+    attach: function (ontext, settings) {
+      var statusMessagesRegion = $('#region--status_messages');
+      var successMessages = statusMessagesRegion.find('.uk-alert-success');
+      var warningMessages = statusMessagesRegion.find('.uk-alert-warning');
+      var dangerMessages = statusMessagesRegion.find('.uk-alert-danger');
 
-  if (successMessages.length) {
-    UIkit.util.on('.uk-alert-success', 'hidden', function (e) {
-      $(e.target).parent('.status-messages').remove();
-      removeMessagesRegion();
-    });
-  }
+      if (successMessages.length) {
+        UIkit.util.on('.uk-alert-success', 'hidden', function (e) {
+          $(e.target).parent('.status-messages').remove();
+          removeMessagesRegion();
+        });
+      }
 
-  if (warningMessages.length) {
-    UIkit.util.on('.uk-alert-warning', 'hidden', function (e) {
-      $(e.target).parent('.status-messages').remove();
-      removeMessagesRegion();
-    });
-  }
+      if (warningMessages.length) {
+        UIkit.util.on('.uk-alert-warning', 'hidden', function (e) {
+          $(e.target).parent('.status-messages').remove();
+          removeMessagesRegion();
+        });
+      }
 
-  if (dangerMessages.length) {
-    UIkit.util.on('.uk-alert-danger', 'hidden', function (e) {
-      $(e.target).parent('.status-messages').remove();
-      removeMessagesRegion();
-    });
-  }
+      if (dangerMessages.length) {
+        UIkit.util.on('.uk-alert-danger', 'hidden', function (e) {
+          $(e.target).parent('.status-messages').remove();
+          removeMessagesRegion();
+        });
+      }
 
-  function removeMessagesRegion() {
-    if (statusMessagesRegion.find('.status-messages').length === 0) {
-      statusMessagesRegion.remove();
+      function removeMessagesRegion() {
+        if (statusMessagesRegion.find('.status-messages').length === 0) {
+          statusMessagesRegion.remove();
+        }
+      }
     }
-  }
+  };
 })(jQuery, Drupal);
